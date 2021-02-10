c969dff70a4c5868a700e14fb6a8e32ab03f8e66
Anweshi <anweshianavadya@gmail.com>
2021-02-10 01:31:52 -0500

    Marketplace 1st update

--- platform4/src/components/Cards.js
+++ platform4/src/components/Cards.js
@@ -13,41 +13,39 @@ function Cards() {
               src='https://i.ytimg.com/vi/ZatiWqQjU7M/maxresdefault.jpg'
               name='Lebron James'
               team="Los Angeles Lakers"
-              position="Small Forward"
+              position="SF"
               label='All-Star'
               path='/services'
             />
             <CardItem
-              src='https://i.ytimg.com/vi/ZatiWqQjU7M/maxresdefault.jpg'
-              name='Lebron James'
-              team="Los Angeles Lakers"
-              position="Small Forward"
-              label='All-Star'
-              path='/services'
-            />
-          </ul>
-          <ul className='cards__items'>
-            <CardItem
-              src='https://i.ytimg.com/vi/ZatiWqQjU7M/maxresdefault.jpg'
-              name='Lebron James'
-              team="Los Angeles Lakers"
-              position="Small Forward"
+              src='https://pbs.twimg.com/profile_images/1140381676306145280/P4LOFLBQ.jpg'
+              name='Kyle Lowry'
+              team="Toronto Raptors"
+              position="PG"
               label='All-Star'
               path='/services'
             />
             <CardItem
-              src='https://i.ytimg.com/vi/ZatiWqQjU7M/maxresdefault.jpg'
-              name='Lebron James'
-              team="Los Angeles Lakers"
-              position="Small Forward"
+              src='https://i.imgflip.com/vvoo7.jpg'
+              name='Russell Westbrook'
+              team="Washington Wizards"
+              position="PG"
               label='All-Star'
               path='/services'
             />
             <CardItem
-              src='https://i.ytimg.com/vi/ZatiWqQjU7M/maxresdefault.jpg'
-              name='Lebron James'
+              src='http://i.imgur.com/viaQFvq.jpg?1'
+              name='Anthony Davis'
               team="Los Angeles Lakers"
-              position="Small Forward"
+              position="PF"
+              label='All-Star'
+              path='/services'
+            />
+            <CardItem
+              src='https://ftw.usatoday.com/wp-content/uploads/sites/90/2019/02/anizj8.png?w=1000&h=600&crop=1'
+              name='Stephen Curry'
+              team="Golden State Warriors"
+              position="SG"
               label='All-Star'
               path='/services'
             />

78cf851b16e3199cbf061c3debd7ba8e1e54ba19
Anweshi <anweshianavadya@gmail.com>
2021-02-10 22:06:47 -0500

    transation page

--- contracts/Deployer.sol
+++ contracts/Deployer.sol
@@ -62,13 +62,50 @@ contract MainManager {
         //AMM deployment TODO
     }
     
+<<<<<<< Updated upstream
     function giveDividend(uint256 tokenId) seasonStarted {
         // have dividedn logic w/e     
+=======
+    // paying dividend per share type, as opposed to all share tyes that msg.sender holds
+    function giveDividendPerPlayer(uint256 tokenId) seasonStarted {
+        // have dividedn logic w/e   
+        // so msg.sender is person who has shares
+        // grab list of top 20 players of the last week (let's go by ppg for now) 
+        // add up the ppg and assign a fraction of dividend fund based on ppg share of pool for each player
+        // grab how many shares of this player there are owned 
+        // grab how many shares of this player msg.sender owns
+        // (sender shares per player / total shares per player) x amount reserved for player from dividend fund, send this back
+
+>>>>>>> Stashed changes
         
         require((lastDividendWithdrawn[msg.sender] + 7 days) < block.timestamp);
         lastDividendWithdrawn[msg.sender] = block.timestamp;
         // ISHAN adds dividend logic
+<<<<<<< Updated upstream
     }
+=======
+        // can
+    }
+
+    // function that will trigger end of auction, and set every value in lastDividendWithdrawn to current date
+    function endAuction() public auctionOngoing onlyOwner {
+        auctionEndDate = block.timestamp
+
+        for (uint i=0; i<shareholders.length; i++) {
+            lastDividendWithdrawn(shareholders[i]) = block.timestamp
+        }
+    }
+
+
+    // need function that calculates and sets dividendFund values every week
+    // also sets currWeekStart
+    function weekTrigger() public onlyOwner [
+
+
+        lastWeekDividendFund = currWeekDividendFund
+        currWeekDividendFund = 0
+    ]
+>>>>>>> Stashed changes
 
     // ISHAN adds more dividend logic
      

c3c5c700b85aca6dbdd0318f09578d718d471c35
Sam O <sam.orend22@gmail.com>
2021-02-11 02:37:39 -0500

    added react states to get js info for buy/sell

--- platform4/src/components/PurchaseCardItem.js
+++ platform4/src/components/PurchaseCardItem.js
@@ -3,49 +3,71 @@ import { Link } from 'react-router-dom';
 import { Button } from './MrktButton';
 import './PurchaseCardItem.css';
 
-function PurchaseCardItem(props) {
+class PurchaseCardItem extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {numShares: '', buy: true, sell: true};
+    
+        this.handleChange = this.handleChange.bind(this);
+        this.confirmBuy = this.confirmBuy.bind(this);
+        this.confirmSell = this.confirmSell.bind(this);
+      }
+    
+      handleChange(event) {
+        this.setState({numShares: event.target.value});
+        localStorage.setItem('pricePerShare', event.target.value);
+      }
 
-  function passTransactionData() {
-    localStorage.setItem('playerPurchaseName', props.name);
-    localStorage.setItem('playerPurchaseTeam', props.team);
-    localStorage.setItem('playerPurchasePos', props.position);
-    localStorage.setItem('playerPurchasePrice', props.price);
-    localStorage.setItem('playerPurchaseImg', props.src);
-    localStorage.setItem('playerPurchaseLabel', props.label);
-  }
+      // KEY NOTE: IF BOTH BUY AND SELL ARE TRUE THIS IS INVALID CASE, CAN FIX TMR
 
-  return (
-    <>
-      <li className='cards__item'>
-        <div className='cards__item__link'>
-          <figure className='cards__item__pic-wrap' data-category={props.label}>
-            <img
-              className='cards__item__img'
-              alt='Travel Image'
-              src={props.src}
-            />
-          </figure>
-          <div className='cards__item__info'>
-            <h5 className='cards__item__name'>{props.name}</h5>
-            <h5 className='cards__item__team'>Team: {props.team}</h5>
-            <h5 className='cards__item__position'>Position: {props.position}</h5>
-            <h5 className='cards__item__position'>Market Price: {props.price}</h5>
-          </div>
-          <div className="shares-quantity">
-            <h5 className='cards__item__name'>Enter Share Quantity</h5>
-            <center><input className="quantity-input" type="number" min="0" max="1000"></input></center>
-            <br></br>
-          </div>
-          <div className="cards_item_buy-sell">
-            <input type="radio" id="buy" name="gender" value="male"></input>
-            <label for="buy">Buy</label><br></br>
-            <input type="radio" id="sell" name="gender" value="male"></input>
-            <label for="sell">Sell</label><br></br>
-          </div>
-        </div>
-      </li>
-    </>
-  );
+      confirmBuy() {
+          this.setState({
+              buy: true
+          });
+          console.log(this.state.buy);
+      }
+
+      confirmSell() {
+        this.setState({
+            sell: true
+        });
+        console.log(this.state.sell);
+      }
+
+    render() {
+        return (
+            <>
+              <li className='cards__item'>
+                <div className='cards__item__link'>
+                  <figure className='cards__item__pic-wrap' data-category={this.props.label}>
+                    <img
+                      className='cards__item__img'
+                      alt='Travel Image'
+                      src={this.props.src}
+                    />
+                  </figure>
+                  <div className='cards__item__info'>
+                    <h5 className='cards__item__name'>{this.props.name}</h5>
+                    <h5 className='cards__item__team'>Team: {this.props.team}</h5>
+                    <h5 className='cards__item__position'>Position: {this.props.position}</h5>
+                    <h5 className='cards__item__position'>Market Price: {this.props.price}</h5>
+                  </div>
+                  <div className="shares-quantity">
+                    <h5 className='cards__item__name'>Enter Share Quantity</h5>
+                    <center><input className="quantity-input" type="number" min="0" max="1000" onChange={this.handleChange}></input></center>
+                    <br></br>
+                  </div>
+                  <div className="cards_item_buy-sell">
+                    <input type="radio" id="buy" name="gender" value="male" onClick={this.confirmBuy}></input>
+                    <label for="buy">Buy</label><br></br>
+                    <input type="radio" id="sell" name="gender" value="male" onClick={this.confirmSell}></input>
+                    <label for="sell">Sell</label><br></br>
+                  </div>
+                </div>
+              </li>
+            </>
+          );
+    }
 }
 
 export default PurchaseCardItem;

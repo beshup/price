9ce171114b9852250bf49aef1c781e5514390eb5
Sam O <sam.orend22@gmail.com>
2021-02-12 02:15:50 -0500

    improved donation page

--- platform4/src/components/DonateCard.js
+++ platform4/src/components/DonateCard.js
@@ -2,69 +2,64 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from './MrktButton';
 import './DonateCard.css';
+import Loading from './Loading';
 
-class PurchaseCardItem extends React.Component {
+class DonateCardItem extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {numShares: '', buy: true, sell: true};
-    
-        this.handleChange = this.handleChange.bind(this);
-        this.confirmBuy = this.confirmBuy.bind(this);
-        this.confirmSell = this.confirmSell.bind(this);
-      }
-    
-      handleChange(event) {
-        this.setState({numShares: event.target.value});
-        localStorage.setItem('pricePerShare', event.target.value);
-        this.props.setQuantity(event.target.value);
-      }
+        this.state = {
+            error: null,
+            isLoaded: false,
+            items: [],
+            donated: 0,
+            waterProjectAddress: '0x5389e39821edc01bb5f6e4b42685c9b9516e1e52',
+            coolEarthAddress:'0x3c8cB169281196737c493AfFA8F49a9d823bB9c5',
+            saveTheChildrenAddress:'0xA0780a008D9CeFC58821Ca3E05dFE1173087C3C4',
+        };
+        this.handleDonation = this.handleDonation.bind(this);
+    }
 
-      // KEY NOTE: IF BOTH BUY AND SELL ARE TRUE THIS IS INVALID CASE, CAN FIX TMR
-
-      confirmBuy() {
-          this.setState({
-              buy: true
-          });
-          console.log(this.state.buy);
-      }
-
-      confirmSell() {
+    handleDonation() {
         this.setState({
-            sell: true
-        });
-        console.log(this.state.sell);
-      }
+            donated:1,
+        })
+    }
 
     render() {
         return (
             <>
-              <li className='cards__item'>
-                <a className='link_website' href={this.props.path}>
-                <div className='cards__item__link'>
-                  <figure className='cards__item__pic-wrap' data-category={this.props.label}>
-                    <img
-                      className='cards__item__img'
-                      alt='Travel Image'
-                      src={this.props.src}
-                    />
-                  </figure>
-                  <div className='cards__item__info'>
-                    <h5 className='cards__item__name'>{this.props.name}</h5>
-                    <h5 className='cards__item__team'>{this.props.team}</h5>
-                    <h5 className='cards__item__position'>Click here to know more about this{this.props.position}</h5>
-                    <h5 className='cards__item__position'>{this.props.price}</h5>
-                  </div>
-                  <div className="shares-quantity">
-                    <h5 className='cards__item__name'>Enter purchase amount</h5>
-                    <center><input className="quantity-input" type="number" min="0" max="1000" onChange={this.handleChange}></input></center>
-                    <br></br>
-                  </div>
-                  </div>
-                </a>
-              </li>
+            {this.state.donated === 0 && 
+
+            <li className='cards__item'>
+            <Link className='cards__item__link' to={this.props.path}>
+            <figure className='cards__item__pic-wrap' data-category={this.props.label}>
+                <img
+                className='cards__item__img'
+                alt='Travel Image'
+                src={this.props.src}
+                />
+            </figure>
+            <div className='cards__item__info'>
+                <h5 className='cards__item__name'>{this.props.name}</h5>
+                <h5 className='cards__item__pos'>{this.props.description}</h5>
+                <h5 className='cards__item__pos'>Give 0.4 ETH Now {this.props.team}</h5>
+                <center><Button buttonStyle='MrktButton--buy' onClick={this.handleDonation}>Donate</Button></center>
+            </div>
+            </Link>
+            </li>
+
+            }
+            {this.state.donated === 1 && <Loading></Loading>}
+            {this.state.donated === 2 &&
+            <div className="donation-complete-top">
+                <h4>We just sent 0.4 ETH to address</h4>
+            </div> 
+            
+            }
             </>
           );
     }
 }
 
-export default PurchaseCardItem;
+export default DonateCardItem;
+

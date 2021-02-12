a3ad859334de67aaa6706dafe5e99e6550ad65c8
Sam O <sam.orend22@gmail.com>
2021-02-11 23:50:19 -0500

    added donate page

---
+++ platform4/src/components/DonateCard.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from './MrktButton';
+import './CardItem.css';
+
+function DonateCardItem(props) {
+
+  function passTransactionData() {
+    localStorage.setItem('playerPurchaseName', props.name);
+    localStorage.setItem('playerPurchaseTeam', props.team);
+    localStorage.setItem('playerPurchasePos', props.position);
+    localStorage.setItem('playerPurchasePrice', props.price);
+    localStorage.setItem('playerPurchaseImg', props.src);
+    localStorage.setItem('playerPurchaseLabel', props.label);
+  }
+
+  return (
+    <>
+      <li className='cards__item'>
+        <Link className='cards__item__link' to={props.path}>
+          <figure className='cards__item__pic-wrap' data-category={props.label}>
+            <img
+              className='cards__item__img'
+              alt='Travel Image'
+              src={props.src}
+            />
+          </figure>
+          <div className='cards__item__info'>
+            <h5 className='cards__item__name'>{props.name}</h5>
+            <h5 className='cards__item__pos'>Team: {props.team}</h5>
+            <h5 className='cards__item__pos'>Position: {props.position}</h5>
+            <h5 className='cards__item__pos'>Market Price: {props.price}</h5>
+          </div>
+          <div className="cards_item_buy-sell">
+            <Button buttonStyle='MrktButton--buy' onClick={passTransactionData}>Donate</Button>
+          </div>
+        </Link>
+      </li>
+    </>
+  );
+}
+
+export default DonateCardItem;

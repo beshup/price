76c3d9eb6d8d81a2bad7f2e0487424899f83a751
Sam O <sam.orend22@gmail.com>
2021-03-09 23:10:35 -0500

    I think better frontend

--- platform4/src/components/CardItem.js
+++ platform4/src/components/CardItem.js
@@ -29,8 +29,8 @@ class CardItem extends React.Component {
                 src={this.props.src}
               />
             </figure>
-            <div className='cards__item__info'>
               <h5 className='cards__item__name'>{this.props.title}</h5>
+              <div className='cards__item__info'>
               {this.props.r1 && <h5 className='cards__item__pos'>{this.props.r1}</h5>}
               {this.props.r2 && <h5 className='cards__item__pos'>{this.props.r2}</h5>}
               {this.props.r3 &&  <h5 className='cards__item__pos'>{this.props.r3}</h5>}
@@ -40,14 +40,14 @@ class CardItem extends React.Component {
                 height={70}
                 options={this.props.graphOptions}
               />}
-            </div>
-            {this.props.btn2Title && <div className="cards_item_buy-sell">
+              {this.props.btn2Title && <div className="cards_item_buy-sell">
               <Button buttonStyle='MrktButton--buy' onClick={this.props.action1Handler}>{this.props.btn1Title}</Button>
               <Button buttonStyle='MrktButton--sell' onClick={this.props.action2Handler}>{this.props.btn2Title}</Button>
             </div>}
-            {this.props.mainBtnText && <div className="cards_item_buy-sell">
+            {this.props.mainBtnText && <div className="cards__item__pos">
               <button className="card_item_main_button" onClick={this.props.mainBtnHandler}>{this.props.mainBtnText}</button>
             </div>}
+            </div>
           </Link>}
         </li>
       </>
